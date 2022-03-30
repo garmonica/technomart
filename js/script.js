4e1b37519ae3afeee6e492eca9f0629e36d462a9
Olia <good-friday@mail.ru>
2022-03-30 12:57:44 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+var link = document.querySelector(".write-us");
+var popup = document.querySelector(".modal-feedback");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector("form");
+var yourname = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+var message = popup.querySelector("[name=message]");
+var storage = localStorage.getItem("yourname");
+
+link.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("modal-show");
+	if (storage) {
+		yourname.value = storage;
+		email.focus();
+	} else {
+		yourname.focus();
+	}
+});
+
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+	if (!yourname.value || !email.value || !message.value) {
+		evt.preventDefault();
+		popup.classList.remove("modal-error");
+		popup.offsetWidth = popup.offsetWidth;
+		popup.classList.add("modal-error");
+	} else {
+		localStorage.setItem("name", yourname.value);
+	}
+});
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		if (popup.classList.contains("modal-show")) {
+			popup.classList.remove("modal-show");
+			popup.classList.remove("modal-error");
+		}
+	}
+});
+
+var mapLink = document.querySelector(".map-open");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+mapLink.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		if (mapPopup.classList.contains("modal-show")) {
+			mapPopup.classList.remove("modal-show");
+		}
+	}
+});
